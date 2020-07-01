33a5f656cc5e521009ec632656681d01128b6766
Tharani718 <tharani12ia@gmail.com>
2020-07-01 13:56:25 +0530

    Simple Stack Implementation

---
+++ Stack/Stack.java
@@ -0,0 +1,37 @@
+public class Stack<T> implements Iterable <T> {
+    private java.util.LinkedList <T> list = new java.util.LinkedList <T>();
+
+    public Stack() { }
+
+    public Stack(T firstElem){
+        push(firstElem);
+    }
+
+    public int size(){
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void push(T elem){
+        list.addLast(elem);
+    }
+
+    public T pop(){
+        if(isEmpty()) throw new java.util.EmptyStackException();
+        return list.removeLast();
+    }
+
+    public T peek(){
+        if(isEmpty()){
+            throw new java.util.EmptyStackException();
+        }
+        return list.peekLast();
+    }
+
+    @Override public java.util.Iterator <T> iterator(){
+        return list.iterator();
+    }
+}
